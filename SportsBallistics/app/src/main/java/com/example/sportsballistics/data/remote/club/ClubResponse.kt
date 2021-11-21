2e3216d0339762009b2278eeee4a996198a9367d
muhammad.hamza <muhammad00hamza@gmail.com>
2021-11-22 00:39:09 +0500

    club list fragment

---
+++ SportsBallistics/app/src/main/java/com/example/sportsballistics/data/remote/club/ClubResponse.kt
@@ -0,0 +1,19 @@
+package com.example.sportsballistics.data.remote.club
+
+data class ClubResponse(
+	val total: Int? = null,
+	val start: Int? = null,
+	val end: Int? = null,
+	val content: Content? = null
+)
+
+data class UsersItem(
+	val name: String? = null,
+	val id: String? = null
+)
+
+data class Content(
+	val headers: List<String?>? = null,
+	val users: List<UsersItem?>? = null
+)
+
