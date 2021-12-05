00516772bc59def45dbfbac5b547db68b6c66b42
muhammad.hamza <muhammad00hamza@gmail.com>
2021-12-06 02:06:44 +0500

    working on APIs

--- SportsBallistics/app/src/main/java/com/example/sportsballistics/ui/dashboard/users/UserFragment.kt
+++ SportsBallistics/app/src/main/java/com/example/sportsballistics/ui/dashboard/users/UserFragment.kt
@@ -8,7 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.afollestad.materialdialogs.MaterialDialog
 import com.example.sportsballistics.R
 import com.example.sportsballistics.data.api.URLIdentifiers
 import com.example.sportsballistics.data.listeners.Listeners
@@ -17,6 +19,7 @@ import com.example.sportsballistics.data.remote.club.UsersItem
 import com.example.sportsballistics.databinding.FragmentUserBinding
 import com.example.sportsballistics.ui.dashboard.dashboard.ClubListAdapter
 import com.example.sportsballistics.ui.dashboard.dashboard.DashboardViewModel
+import com.example.sportsballistics.utils.AppConstant
 import com.google.gson.Gson
 
 class UserFragment : Fragment()
@@ -91,9 +94,43 @@ class UserFragment : Fragment()
             {
                 //TODO Asher bind data with ui
                 Log.d(UserFragment::class.simpleName,Gson().toJson(content))
-//                initRecyclerView(content.content?.users as MutableList<UsersItem>)
-                initRecyclerView()
+                initRecyclerView(content.content?.users as MutableList<UsersItem>)
             }
         })
     }
+
+    private fun initRecyclerView(list: MutableList<UsersItem>) {
+        val mLayoutManager = LinearLayoutManager(context)
+        val mAdapter = com.example.sportsballistics.ui.dashboard.club.ClubListAdapter(context, list, object :
+                com.example.sportsballistics.ui.dashboard.club.ClubListAdapter.OnItemClickListener
+        {
+            override fun onEditClick(adapterType: Int, user: UsersItem)
+            {
+                val args = Bundle()
+                args.putString(AppConstant.INTENT_EXTRA_1, user.id)
+                args.putInt(AppConstant.INTENT_SCREEN_TYPE, AppConstant.INTENT_SCREEN_TYPE_EDIT)
+                Navigation.findNavController(binding.root).navigate(R.id.action_clubFragment_to_createClubFragment, args)
+            }
+
+            override fun onViewClick(adapterType: Int, user: UsersItem)
+            {
+                val args = Bundle()
+                args.putString(AppConstant.INTENT_EXTRA_1, user.id)
+                args.putInt(AppConstant.INTENT_SCREEN_TYPE, AppConstant.INTENT_SCREEN_TYPE_VIEW)
+                Navigation.findNavController(binding.root).navigate(R.id.action_clubFragment_to_createClubFragment, args)
+            }
+
+            override fun onDeleteClick(adapterType: Int, user: UsersItem)
+            {
+                MaterialDialog(binding.root.context).title(null, "Want to delete!").message(null, "Do you want to delete this Club?").positiveButton(null, "YES") {
+
+                }.negativeButton(null, "NO") {
+
+                }.show()
+            }
+        })
+        binding.recyclerView.layoutManager = mLayoutManager
+        binding.recyclerView.adapter = mAdapter
+    }
+
 }
