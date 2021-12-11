1f66aeec363d96713a9419a76a1cf6a70cc03a4a
muhammad.hamza <muhammad00hamza@gmail.com>
2021-12-11 20:49:01 +0500

    APi interface conflicts fixed

--- SportsBallistics/app/src/main/java/com/example/sportsballistics/data/api/ApiInterface.java
+++ SportsBallistics/app/src/main/java/com/example/sportsballistics/data/api/ApiInterface.java
@@ -2,6 +2,7 @@ package com.example.sportsballistics.data.api;
 
 
 import static com.example.sportsballistics.data.api.URLIdentifiers.DELETE_USER;
+import static com.example.sportsballistics.data.api.URLIdentifiers.SKILL_POST;
 
 import com.example.sportsballistics.data.remote.AthleteResponse;
 import com.example.sportsballistics.data.remote.DashboardModel;
@@ -14,11 +15,13 @@ import com.example.sportsballistics.data.remote.login.UserResponse;
 import com.example.sportsballistics.data.remote.service.ServiceResponseModel;
 
 import java.util.List;
+import java.util.Map;
 
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
@@ -36,12 +39,12 @@ public interface ApiInterface {
 
     @POST(URLIdentifiers.GET_ALL_CLUBS)
     @FormUrlEncoded
-    Call<ClubResponse> getContent(@Field("select_limit") int limit, @Field("action") String action);
+    Call<ClubResponse> getContent(@Field("select_limit") int limit, @Field("action") String action, @Field("search_text") String search_text);
 
     @Headers("Content-Type:application/x-www-form-urlencoded")
     @POST(URLIdentifiers.GET_ALL_CLUBS)
     @FormUrlEncoded
-    Call<GenericResponse> getMainContent(@Field("select_limit") int limit, @Field("action") String action);
+    Call<GenericResponse> getMainContent(@Field("select_limit") int limit, @Field("action") String action, @Field("search_text") String search_text);
 
     @POST(URLIdentifiers.GET_DASHBOARD)
     Call<DashboardResponse> getDashboard();
@@ -80,12 +83,12 @@ public interface ApiInterface {
     @POST(URLIdentifiers.EDIT_CLUB)
     @FormUrlEncoded
     Call<DashboardModel> editClub(@Path(value = "club_id", encoded = true) String id,
-                                  @Field("name") String name,
-                                  @Field("address") String address,
-                                  @Field("state") String state,
-                                  @Field("city") String city,
-                                  @Field("status") String status,
-                                  @Field("zipcode") int zipcode);
+            @Field("name") String name,
+            @Field("address") String address,
+            @Field("state") String state,
+            @Field("city") String city,
+            @Field("status") String status,
+            @Field("zipcode") int zipcode);
 
     @POST(URLIdentifiers.VIEW_CLUB)
     Call<ViewClubResponse> viewClub(@Path(value = "club_id", encoded = true) String id);
@@ -97,35 +100,38 @@ public interface ApiInterface {
     @POST(URLIdentifiers.ADD_USER)
     @FormUrlEncoded
     Call<DashboardModel> addTrainer(@Field("email") int email,
-                                    @Field("fullname") int fullname,
-                                    @Field("contact_no") int contact_no,
-                                    @Field("age") int age,
-                                    @Field("state") int state,
-                                    @Field("zipcode") int zipcode,
-                                    @Field("city") int limit,
-                                    @Field("status") int status,
-                                    @Field("address") int address,
-                                    @Field("grade") int grade,
-                                    @Field("password") int password,
-                                    @Field("package_type") int package_type,
-                                    @Field("club_name") int club_name,
-                                    @Field("role_id") int role_id); //role will be for only role 4
-
-    @POST(URLIdentifiers.EDIT_CLUB)
+            @Field("fullname") int fullname,
+            @Field("contact_no") int contact_no,
+            @Field("age") int age,
+            @Field("state") int state,
+            @Field("zipcode") int zipcode,
+            @Field("city") int limit,
+            @Field("status") int status,
+            @Field("address") int address,
+            @Field("grade") int grade,
+            @Field("password") int password,
+            @Field("package_type") int package_type,
+            @Field("club_name") int club_name,
+            @Field("role_id") int role_id); //role will be for only role 4
+    @FormUrlEncoded
+    @POST(URLIdentifiers.ADD_USER)
     Call<DashboardModel> addTrainer(@Field("email") String email,
-                                    @Field("fullname") String fullname,
-                                    @Field("contact_no") String contact_no,
-                                    @Field("age") String age,
-                                    @Field("state") String state,
-                                    @Field("zipcode") int zipcode,
-                                    @Field("city") String city,
-                                    @Field("status") String status,
-                                    @Field("address") String address,
-                                    @Field("grade") String grade,
-                                    @Field("password") String password,
-                                    @Field("package_type") String package_type,
-                                    @Field("club_name") String club_name,
-                                    @Field("role_id") String role_id); //role will be for only role 4
+            @Field("fullname") String fullname,
+            @Field("contact_no") String contact_no,
+            @Field("age") String age,
+            @Field("state") String state,
+            @Field("zipcode") int zipcode,
+            @Field("city") String city,
+            @Field("status") String status,
+            @Field("address") String address,
+            @Field("grade") String grade,
+            @Field("password") String password,
+            @Field("package_type") String package_type,
+            @Field("club_name") String club_name,
+            @Field("role_id") String role_id);
+
+    @Multipart
+    @POST(URLIdentifiers.ADD_USER)
     Call<DashboardModel> addTrainer(@Query("email") String email,
             @Query("fullname") String fullname,
             @Query("contact_no") String contact_no,
@@ -145,19 +151,19 @@ public interface ApiInterface {
     @POST(URLIdentifiers.EDIT_USER)
     @FormUrlEncoded
     Call<DashboardModel> editTrainer(@Path(value = "userid", encoded = true) String id, @Field("email") String email,
-                                     @Field("fullname") String fullname,
-                                     @Field("contact_no") String contact_no,
-                                     @Field("age") String age,
-                                     @Field("state") String state,
-                                     @Field("zipcode") int zipcode,
-                                     @Field("city") String city,
-                                     @Field("status") String status,
-                                     @Field("address") String address,
-                                     @Field("grade") String grade,
-                                     @Field("password") String password,
-                                     @Field("package_type") String package_type,
-                                     @Field("club_name") String club_name,
-                                     @Field("role_id") String role_id);
+            @Field("fullname") String fullname,
+            @Field("contact_no") String contact_no,
+            @Field("age") String age,
+            @Field("state") String state,
+            @Field("zipcode") int zipcode,
+            @Field("city") String city,
+            @Field("status") String status,
+            @Field("address") String address,
+            @Field("grade") String grade,
+            @Field("password") String password,
+            @Field("package_type") String package_type,
+            @Field("club_name") String club_name,
+            @Field("role_id") String role_id);
 
     @POST(URLIdentifiers.VIEW_USER)
     Call<AthleteResponse> viewTrainer(@Path(value = "userid", encoded = true) String id);
@@ -172,9 +178,11 @@ public interface ApiInterface {
     @GET(URLIdentifiers.SERVICE_SLUG_DETAILS)
     Call<FormServiceModel> getFormInfo(@Path("SLUG") String slug, @Path("USER_ID") String path);
 
-//    Call<ServiceResponseModel> getServiceContent();
-
-
+    @FormUrlEncoded
+    @POST(SKILL_POST)
+    Call<DashboardModel> submitSkillForm(@Path("SLUG") String slug, @Path("USER_ID") String id,
+            @FieldMap Map<String, String> params
+    );
 }
 
 
