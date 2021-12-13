00895b94fc81c864cbfa739692f9efd17317dedb
muhammad.hamza <muhammad00hamza@gmail.com>
2021-12-13 23:52:51 +0500

    change password

---
+++ SportsBallistics/app/src/main/java/com/example/sportsballistics/data/remote/AccountResponse.kt
@@ -0,0 +1,36 @@
+package com.example.sportsballistics.data.remote
+
+import com.google.gson.annotations.SerializedName
+
+data class AccountResponse(
+
+	@field:SerializedName("data")
+	val data: List<DataItem?>? = null,
+
+	@field:SerializedName("is_error")
+	val isError: Boolean? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
+
+data class DataItem(
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("updated")
+	val updated: String? = null,
+
+	@field:SerializedName("slug")
+	val slug: String? = null,
+
+	@field:SerializedName("content")
+	val content: String? = null,
+
+	@field:SerializedName("status")
+	val status: String? = null
+)
