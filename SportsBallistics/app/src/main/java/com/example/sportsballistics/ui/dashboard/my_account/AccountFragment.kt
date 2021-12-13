f6ded70f1ae63c752fa847a301cd8f25911bcfea
Muhammad Asher Ansari <asher.ansari@cloudasset.com>
2021-12-14 00:41:01 +0500

    Done adding content working from account fragment to AccountDetailFragment.

--- SportsBallistics/app/src/main/java/com/example/sportsballistics/ui/dashboard/my_account/AccountFragment.kt
+++ SportsBallistics/app/src/main/java/com/example/sportsballistics/ui/dashboard/my_account/AccountFragment.kt
@@ -28,17 +28,19 @@ import com.example.sportsballistics.utils.AppConstant
 import com.example.sportsballistics.utils.add
 import com.google.gson.Gson
 
-class AccountFragment : Fragment()
-{
+class AccountFragment : Fragment() {
     private lateinit var viewModel: AccountViewModel
     lateinit var binding: FragmentAccountBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
-    {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_account, container, false
-                                         );
+        );
         initViewModel()
         binding.root.findViewById<AppCompatImageView>(R.id.backClubList).setOnClickListener {
             Navigation.findNavController(binding.root).navigateUp()
@@ -50,24 +52,26 @@ class AccountFragment : Fragment()
         return binding.root
     }
 
-   private fun setListeners(){
-        binding.rlAboutSportsBallistics.setOnClickListener{ getAccountData("about-us") }
-        binding.rlPP.setOnClickListener{getAccountData("privacy-policy") }
-        binding.rlTerms.setOnClickListener{getAccountData("terms-use") }
-        binding.rlContactSupport.setOnClickListener{getAccountData("contact-support") }
-        binding.rlFAQs.setOnClickListener{getAccountData("faqs ") }
+    private fun setListeners() {
+        binding.rlAboutSportsBallistics.setOnClickListener { getAccountData("about-us") }
+        binding.rlPP.setOnClickListener { getAccountData("privacy-policy") }
+        binding.rlTerms.setOnClickListener { getAccountData("terms-use") }
+        binding.rlContactSupport.setOnClickListener { getAccountData("contact-support") }
+        binding.rlFAQs.setOnClickListener { getAccountData("faqs ") }
 
-       binding.txtChangePassword.setOnClickListener{
+        binding.txtChangePassword.setOnClickListener {
 
-       }
+        }
     }
-    fun setData(){
+
+    fun setData() {
         val user = SharedPrefUtil.getInstance().user
         binding.txtNameValue.setText(user.loggedIn?.fullname)
         binding.txtEmailValue.setText(user.loggedIn?.email)
 //        binding.txtNumberValue.setText(user.loggedIn?.)
         binding.txtRoleValue.setText(user.loggedIn?.roleId)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
@@ -76,32 +80,41 @@ class AccountFragment : Fragment()
         viewModel = ViewModelProviders.of(this).get(AccountViewModel::class.java)
         viewModel.attachErrorListener(object : Listeners.DialogInteractionListener {
             override fun dismissDialog() {
-                binding.progressBar.visibility=View.GONE
+                binding.progressBar.visibility = View.GONE
             }
 
             override fun addDialog() {
-                binding.progressBar.visibility=View.VISIBLE
+                binding.progressBar.visibility = View.VISIBLE
             }
 
             override fun addErrorDialog() {
-                binding.progressBar.visibility=View.GONE
+                binding.progressBar.visibility = View.GONE
             }
 
             override fun addErrorDialog(msg: String?) {
-                binding.progressBar.visibility=View.GONE
-                Toast.makeText(binding.root.context,msg, Toast.LENGTH_SHORT).show()
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(binding.root.context, msg, Toast.LENGTH_SHORT).show()
             }
         })
     }
 
 
-   private fun getAccountData(slug:String){
-        viewModel.getSettings(requireContext(),slug,object:AccountViewModel.ContentFetchListener{
-            override fun onFetched(content: AccountResponse)
-            {
-                Log.d("ACCOUNT",Gson().toJson(content))
-                //TODO Asher please add code for navigation to AccountDetailFragment and add two extra parameter and pass title and body
-            }
-        })
+    private fun getAccountData(slug: String) {
+        viewModel.getSettings(
+            requireContext(),
+            slug,
+            object : AccountViewModel.ContentFetchListener {
+                override fun onFetched(content: AccountResponse) {
+                    Log.d("ACCOUNT", Gson().toJson(content))
+                    binding.progressBar.visibility=View.GONE
+                    if (content.data != null && content.data.size > 0) {
+                        val bundle = Bundle()
+                        bundle.putString(AppConstant.INTENT_EXTRA_1, content.data.get(0)!!.title)
+                        bundle.putString(AppConstant.INTENT_EXTRA_2, content.data.get(0)!!.content)
+                        Navigation.findNavController(binding.root)
+                            .navigate(R.id.action_accountFragment_to_accountDetailFragment, bundle)
+                    }
+                }
+            })
     }
 }
