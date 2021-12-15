e534b72774e35e79efcabbf6bdf1a0f87e2373f0
Muhammad Asher Ansari <asher.ansari@cloudasset.com>
2021-12-16 00:28:44 +0500

    Done working on a splash screen designing changing on a runTime.

--- SportsBallistics/app/src/main/java/com/example/sportsballistics/ui/login/LoginActivity.kt
+++ SportsBallistics/app/src/main/java/com/example/sportsballistics/ui/login/LoginActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.example.sportsballistics.AppSystem
 import com.example.sportsballistics.R
 import com.example.sportsballistics.data.SharedPrefUtil
@@ -29,6 +30,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.login);
+        loadAssets()
 
         initViewModel()
 
@@ -63,12 +65,15 @@ class LoginActivity : AppCompatActivity() {
                                 AppSystem.getInstance().setCurrentUser(userResponse)
                                 binding.btnLogin.revertAnimation();
 
-                                Handler(Looper.getMainLooper()).postDelayed( {
+                                Handler(Looper.getMainLooper()).postDelayed({
                                     launchActivityFinish<DashboardActivity> {
-                                        this.putExtra(URLIdentifiers.USER_ROLE, userResponse.loggedIn?.roleId)
+                                        this.putExtra(
+                                            URLIdentifiers.USER_ROLE,
+                                            userResponse.loggedIn?.roleId
+                                        )
                                     }
                                     finish()
-                                },200)
+                                }, 200)
                             }
                         } else {
                             Toast.makeText(
@@ -102,14 +107,73 @@ class LoginActivity : AppCompatActivity() {
     }
 
     //TODO use this method temp for all asset loading
-    fun loadAssets(){
+    fun loadAssets() {
         val sportsType = SharedPrefUtil.getInstance().sportsType
-        when(sportsType){
-            AppConstant.BASEBALL->{}
-            AppConstant.VOLLEYBALL->{}
-            AppConstant.QB->{}
+        when (sportsType) {
+            AppConstant.BASEBALL -> {
+                Glide.with(binding.root).load(R.drawable.bb_login_bg).into(binding.ivBackground)
+                Glide.with(binding.root).load(R.drawable.bb_inner_logo).into(binding.imgLogo)
+                binding.btnLogin.setBackgroundResource(R.drawable.btn_baseball)
+                AppConstant.getDrawableTintedColor(
+                    R.drawable.ic_username_icon,
+                    R.color.colorBB,
+                    binding.etEmail
+                )
+                AppConstant.getDrawableTintedColor(
+                    R.drawable.ic_password_icon,
+                    R.color.colorBB,
+                    binding.etPassword
+                )
+
+            }
+            AppConstant.VOLLEYBALL -> {
+                Glide.with(binding.root).load(R.drawable.vb_login_bg).into(binding.ivBackground)
+                Glide.with(binding.root).load(R.drawable.vb_inner_logo).into(binding.imgLogo)
+                binding.btnLogin.setBackgroundResource(R.drawable.btn_volleyball)
+                AppConstant.getDrawableTintedColor(
+                    R.drawable.ic_username_icon,
+                    R.color.colorVB,
+                    binding.etEmail
+                )
+                AppConstant.getDrawableTintedColor(
+                    R.drawable.ic_password_icon,
+                    R.color.colorVB,
+                    binding.etPassword
+                )
+            }
+            AppConstant.TODDLER -> {
+                Glide.with(binding.root).load(R.drawable.ic_toddler_login_bg)
+                    .into(binding.ivBackground)
+                Glide.with(binding.root).load(R.drawable.ic_toddler_inner_logo)
+                    .into(binding.imgLogo)
+                binding.btnLogin.setBackgroundResource(R.drawable.btn_bg)
+                AppConstant.getDrawableTintedColor(
+                    R.drawable.ic_username_icon,
+                    R.color.colorTodd,
+                    binding.etEmail
+                )
+                AppConstant.getDrawableTintedColor(
+                    R.drawable.ic_password_icon,
+                    R.color.colorTodd,
+                    binding.etPassword
+                )
+            }
+            AppConstant.QB -> {
+                Glide.with(binding.root).load(R.drawable.qb_login_bg).into(binding.ivBackground)
+                Glide.with(binding.root).load(R.drawable.qb_inner_logo).into(binding.imgLogo)
+                binding.btnLogin.setBackgroundResource(R.drawable.btn_qb)
+                AppConstant.getDrawableTintedColor(
+                    R.drawable.ic_username_icon,
+                    R.color.colorQB,
+                    binding.etEmail
+                )
+                AppConstant.getDrawableTintedColor(
+                    R.drawable.ic_password_icon,
+                    R.color.colorQB,
+                    binding.etPassword
+                )
+            }
         }
-        binding.btnLogin.setBackgroundColor(ContextCompat.getColor(applicationContext,R.color.actionBarColor))
     }
 
 }
