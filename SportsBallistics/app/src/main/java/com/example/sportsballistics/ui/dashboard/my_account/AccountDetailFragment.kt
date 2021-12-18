a9c2a99e534f09d211c98ce1d08aee7af82173d7
muhammad.hamza <muhammad00hamza@gmail.com>
2021-12-18 23:33:42 +0500

    fixes

--- SportsBallistics/app/src/main/java/com/example/sportsballistics/ui/dashboard/my_account/AccountDetailFragment.kt
+++ SportsBallistics/app/src/main/java/com/example/sportsballistics/ui/dashboard/my_account/AccountDetailFragment.kt
@@ -7,11 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
-import com.example.sportsballistics.AppSystem
 import com.example.sportsballistics.R
 import com.example.sportsballistics.data.SharedPrefUtil
 import com.example.sportsballistics.databinding.FragmentAccountDetailBinding
